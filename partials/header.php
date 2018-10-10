1c3a62b678b85053db2684dc025cd2ef3597f4ed
RafalZwolak <nedvedek111@gmail.com>
2018-10-10 20:45:15 +0200

    Add subsites

--- partials/header.php
+++ partials/header.php
@@ -1,37 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?php echo $pageDescription; ?>" />
-        <meta name="keywords" content="">
-        <title><?php echo $pageTitle; ?></title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $pageDescription; ?>" />
+    <meta name="keywords" content="">
+    <title><?php echo $pageTitle; ?></title>
 
-        <!-- CSS -->
-        <link rel="stylesheet" type="text/css" href="<?php echo $assets; ?>/dist/css/normalize.css">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $assets; ?>/dist/css/normalize.css">
 
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
-        
-        <link rel="stylesheet" type="text/css" href="<?php echo $assets; ?>/dist/css/main.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
+    
+    <link rel="stylesheet" type="text/css" href="<?php echo $assets; ?>/dist/css/main.css">
 
-        <!-- FavIcon -->
-        <link rel="icon" type="image/png" href="<?php echo $assets; ?>/img/favicon.png" sizes="32x32">
+    <!-- FavIcon -->
+    <link rel="icon" type="image/png" href="<?php echo $assets; ?>/img/favicon.png" sizes="32x32">
 
-        <!-- Fonts -->
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
-        <!-- JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    </head>
-    <body>
-        
-        <?php echo $mainFolder ?>
-        <nav id="menu">        
-            <div class="language">
-                <a href="" class="is-active">PL</a>
-            </div>
-        </nav>
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <nav id="menu">        
+      <a href="./portfolio.php">Portfolio</a>
+      <a href="./experiance.php">Experiance</a>
+      <a href="./contact.php">Contact</a>
+    </nav>
 
         
         
