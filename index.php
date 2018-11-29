07e7d895654e46664655743785316581a4b56342
RafalZwolak <nedvedek111@gmail.com>
2018-11-29 08:26:43 +0100

    Fix php code for templating videos an projects

--- index.php
+++ index.php
@@ -8,29 +8,17 @@
   <div class="full-height-container" id="portfolio">
     <div class="portfolio-container">
       <?php
-        $query = [20, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26];
-        $query = [20, 11, 13, 12, 4, 23, 18, 1, 14, 2, 21, 16, 22, 17, 15, 25, 19, 3, 5, 24, 6, 7, 8, 9, 10];
-
-        // display thumbnails for all projects
-        $assistProjects = [6, 7, 8, 9, 10];
+        $queryProjects = [20, 11, 13, 12, 4, 23, 18, 1, 14, 2, 21, 16, 22, 17, 15, 25, 19, 3, 5, 24];
 
         $directory = "./img/projects/";
-        $filecount = count(glob($directory."*"));
+        // $filecount = count(glob($directory."*"));
 
-        for ($i = 1; $i <= $filecount; $i++) {
-          $assistFlag = false;
-          for ($j = 0; $j < count($assistProjects); $j++)
-            if ($assistProjects[$j] === $query[$i - 1]) {
-              $assistFlag = true;
-            }
-        
-          if (!$assistFlag) {
-            echo 
-            "<a href='$files/project.php?id=".$query[$i - 1]."' class='portfolio-item'>
-              <div class='portfolio-hint'>click to view project</div>
-              <img src='$assets/img/projects/".$query[$i - 1]."/1.jpg' >
-            </a>";
-          }
+        for ($i = 0; $i < count($queryProjects); $i++) {
+          echo 
+          "<a href='$files/project.php?id=".$queryProjects[$i]."' class='portfolio-item'>
+            <div class='portfolio-hint'>click to view project</div>
+            <img src='$assets/img/projects/".$queryProjects[$i]."/1.jpg' >
+          </a>";
         }
 
       ?>
@@ -38,36 +26,30 @@
     
     <p class="assistant menu-link">Videos</p>
     <div class="portfolio-container">
-      <div class="portfolio-item video">
-        <iframe allowfullscreen="allowfullscreen"
-          src="https://www.youtube.com/embed/94sXFayuUXg">
-        </iframe>
-      </div>
-      
-      <div class="portfolio-item video">
-        <iframe allowfullscreen="allowfullscreen"
-          src="https://www.youtube.com/embed/pl-ecD2Ka-E">
-        </iframe>
-      </div>
+      <?php
+        $videoLinks = ['94sXFayuUXg', 'pl-ecD2Ka-E', 'qA-IoBRpvlA'];
+        for ($i = 0; $i < count($videoLinks); $i++)
 
-      <div class="portfolio-item video">
-        <iframe allowfullscreen="allowfullscreen"
-          src="https://www.youtube.com/embed/qA-IoBRpvlA">
-        </iframe>
-      </div>
+        echo 
+        "<div class='portfolio-item video'>
+          <iframe allowfullscreen
+            src='https://www.youtube.com/embed/".$videoLinks[$i]."'>
+          </iframe>
+        </div>";
+      ?>
     </div>
 
     <p class="assistant menu-link">Assistant</p>
     <div class="portfolio-container">
       <?php
-        // display thumbnails for all projects
+        $assistProjects = [6, 7, 8, 9, 10];
         for ($i = 0; $i < count($assistProjects); $i++)
         
         echo 
         "<a href='$files/project.php?id=".$assistProjects[$i]."' class='portfolio-item'>
           <div class='portfolio-hint'>click to view project</div>
           <img src='$assets/img/projects/".$assistProjects[$i]."/1.jpg' >
-        </a>"
+        </a>";
       ?>
     </div>
   </div>
